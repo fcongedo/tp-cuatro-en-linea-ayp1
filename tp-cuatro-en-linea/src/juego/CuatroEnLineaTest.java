ec5cc8c84e64d9fefdd05971c85c0b709f1151fa
Franco Congedo <franco_congedo@hotmail.com>
2021-07-16 18:09:44 -0300

    Update CuatroEnLineaTest.java

--- tp-cuatro-en-linea/src/juego/CuatroEnLineaTest.java
+++ tp-cuatro-en-linea/src/juego/CuatroEnLineaTest.java
@@ -6,83 +6,125 @@ import org.junit.Test;
 public class CuatroEnLineaTest {
 
 	@Test(expected = Error.class)
+	public void PrueboErrorCrearJugadoresConNombresIguales() {
+
+		new CuatroEnLinea(4, 4, "carlos", "carlos");
+	}
+
+	@Test(expected = Error.class)
+	public void PrueboErrorCrearJugadoresConNombresVacios() {
+		new CuatroEnLinea(4, 4, "", "");
+	}
+
+	@Test(expected = Error.class)
+	public void PrueboErrorCrearJugadorAzulConNombreVacio() {
+		new CuatroEnLinea(4, 4, "", "carlos");
+	}
+
+	@Test(expected = Error.class)
+	public void PrueboErrorCrearJugadorAmarilloConNombreVacio() {
+		new CuatroEnLinea(4, 4, "carlos", "");
+	}
+
+	@Test(expected = Error.class)
 	public void PrueboErrorAlCrearJuegoConValorFilaNegativo() {
 
-		new CuatroEnLinea(-7, 5, "Rojo", "Amarillo");
+		new CuatroEnLinea(-7, 5, "Azul", "Amarillo");
 	}
 
 	@Test(expected = Error.class)
 	public void PrueboErrorAlCrearJuegoConValorColumnaNegativo() {
 
-		new CuatroEnLinea(7, -7, "Rojo", "Amarillo");
+		new CuatroEnLinea(7, -7, "Azul", "Amarillo");
 	}
 
 	@Test(expected = Error.class)
 	public void PrueboErrorAlCrearJuegoConValorFilaYColumnaNegativo() {
 
-		new CuatroEnLinea(-7, -7, "Rojo", "Amarillo");
+		new CuatroEnLinea(-7, -7, "Azul", "Amarillo");
 	}
 
 	@Test(expected = Error.class)
 	public void PrueboErrorAlCrearJuegoConValorUnoEnFilaYColumna() {
 
-		new CuatroEnLinea(1, 1, "Rojo", "Amarillo");
+		new CuatroEnLinea(1, 1, "Azul", "Amarillo");
 	}
 
 	@Test(expected = Error.class)
 	public void PrueboErrorAlCrearJuegoConValorUnoEnFila() {
 
-		new CuatroEnLinea(1, 4, "Rojo", "Amarillo");
+		new CuatroEnLinea(1, 4, "Azul", "Amarillo");
 	}
 
 	@Test(expected = Error.class)
 	public void PrueboErrorAlCrearJuegoConValorUnoColumna() {
 
-		new CuatroEnLinea(4, 1, "Rojo", "Amarillo");
+		new CuatroEnLinea(4, 1, "Azul", "Amarillo");
 	}
 
 	@Test(expected = Error.class)
 	public void PrueboElErrorsiSeCreaUnJuegoConTreFilasYCuatroColumnasDaError() {
 
-		new CuatroEnLinea(3, 4, "Rojo", "Amarillo");
+		new CuatroEnLinea(3, 4, "Azul", "Amarillo");
 	}
 
 	@Test(expected = Error.class)
 	public void PrueboElErrorsiSeCreaUnJuegoConCuatroFilasYTresColumnasDaError() {
 
-		new CuatroEnLinea(4, 3, "Rojo", "Amarillo");
+		new CuatroEnLinea(4, 3, "Azul", "Amarillo");
+	}
+
+	@Test(expected = Error.class)
+	public void PrueboElErrorsiSeCreaUnJuegoConNombresIguales() {
+		new CuatroEnLinea(4, 4, "carlos", "carlos");
+	}
+
+	@Test(expected = Error.class)
+	public void PrueboElErrorsiSeCreaUnJuegoConTreceFilasYTreceColumnasDaError() {
+		// pruebo pasar limite maximo de filas y columnas validas da error
+
+		new CuatroEnLinea(13, 13, "Azul", "Amarillo");
+	}
+
+	@Test(expected = Error.class)
+	public void PrueboElErrorsiSeCreaUnJuegoConTresFilasYTresColumnasDaError() {
+		// pruebo pasar limite maximo de filas y columnas validas da error
+
+		new CuatroEnLinea(3, 3, "Azul", "Amarillo");
 	}
 
 	@Test
 	public void alComenzarElJuegoComprueboQueNoHayGanador() {
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 
 		Assert.assertFalse(juego.hayGanador());
 		Assert.assertNull(juego.obtenerGanador());
 	}
 
 	@Test
-	public void ComprueboLaCorrectaCreacionDeElTableroDeTamañoCuatroXCuatro() {
+	public void ComprueboLaCorrectaCreacionDeElTableroDeTamaÃ±oCuatroXCuatroMinimo() {
+		// pruebo limite minimo de columnas y filas validas
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 
 		Assert.assertEquals(4, juego.contarFilas());
 		Assert.assertEquals(4, juego.contarColumnas());
 	}
 
 	@Test
-	public void ComprueboLaCorrectaCreacionDeElTableroDeTamañoCuarentaXTrenta() {
+	public void ComprueboLaCorrectaCreacionDeElTableroDeTamaÃ±o8X8Maximo() {
+		// pruebo limite maximo de columnas y filas maximas
+		CuatroEnLinea juego = new CuatroEnLinea(8, 8, "Azul", "Amarillo");
 
-		CuatroEnLinea juego = new CuatroEnLinea(40, 30, "Rojo", "Amarillo");
-
-		Assert.assertEquals(40, juego.contarFilas());
-		Assert.assertEquals(30, juego.contarColumnas());
+		Assert.assertEquals(8, juego.contarFilas());
+		Assert.assertEquals(8, juego.contarColumnas());
 	}
 
 	@Test
 	public void ComprueboQueAlCrearElJuegoDichoJuegoNoTermino() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 
 		Assert.assertFalse(juego.termino());
 	}
@@ -90,7 +132,7 @@ public class CuatroEnLineaTest {
 	@Test
 	public void PrueboQueAlCrearElJuegoElTableroSeCreaVacio() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 
 		for (int i = 1; i <= juego.contarFilas(); i++) {
 			for (int j = 1; j <= juego.contarColumnas(); j++) {
@@ -103,114 +145,90 @@ public class CuatroEnLineaTest {
 	@Test
 	public void PrueboQueAlSoltarCuatroFichasEnColumna1VaciaCompletaLaFila() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
+
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
 
 		Assert.assertEquals(Casillero.AMARILLO, juego.obtenerCasillero(1, 1));
-		Assert.assertEquals(Casillero.ROJO, juego.obtenerCasillero(2, 1));
+		Assert.assertEquals(Casillero.AZUL, juego.obtenerCasillero(2, 1));
 		Assert.assertEquals(Casillero.AMARILLO, juego.obtenerCasillero(3, 1));
-		Assert.assertEquals(Casillero.ROJO, juego.obtenerCasillero(4, 1));
+		Assert.assertEquals(Casillero.AZUL, juego.obtenerCasillero(4, 1));
 
 	}
 
 	@Test
 	public void PrueboQueAlSoltarTresFichasEnColumna1VaciaCaeEnLasTresUltimasFilas() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
 
 		Assert.assertEquals(Casillero.VACIO, juego.obtenerCasillero(1, 1));
-		Assert.assertEquals(Casillero.ROJO, juego.obtenerCasillero(2, 1));
+		Assert.assertEquals(Casillero.AZUL, juego.obtenerCasillero(2, 1));
 		Assert.assertEquals(Casillero.AMARILLO, juego.obtenerCasillero(3, 1));
-		Assert.assertEquals(Casillero.ROJO, juego.obtenerCasillero(4, 1));
+		Assert.assertEquals(Casillero.AZUL, juego.obtenerCasillero(4, 1));
 
 	}
 
 	@Test
 	public void PrueboQueAlSoltarDosFichasEnColumna1VaciaCompletaLaFila() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
 
 		Assert.assertEquals(Casillero.VACIO, juego.obtenerCasillero(1, 1));
 		Assert.assertEquals(Casillero.VACIO, juego.obtenerCasillero(2, 1));
 		Assert.assertEquals(Casillero.AMARILLO, juego.obtenerCasillero(3, 1));
-		Assert.assertEquals(Casillero.ROJO, juego.obtenerCasillero(4, 1));
+		Assert.assertEquals(Casillero.AZUL, juego.obtenerCasillero(4, 1));
 
 	}
 
 	@Test
 	public void PrueboQueAlSoltarFichaEnColumna1VaciaCaeEnLaultimaFilaDeLaColumnaUno() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 		juego.soltarFicha(1);
 
 		Assert.assertEquals(Casillero.VACIO, juego.obtenerCasillero(1, 1));
 		Assert.assertEquals(Casillero.VACIO, juego.obtenerCasillero(2, 1));
 		Assert.assertEquals(Casillero.VACIO, juego.obtenerCasillero(3, 1));
-		Assert.assertEquals(Casillero.ROJO, juego.obtenerCasillero(4, 1));
+		Assert.assertEquals(Casillero.AZUL, juego.obtenerCasillero(4, 1));
 
 	}
 
 	@Test
-	public void PrueboQueSiComienzaElRojoYCompletaUnaColumnaDeCuatroCasillerosElProximoTurnoEsElRojo() {
+	public void PrueboQueSiComienzaElAzulYCompletaUnaColumnaDeCuatroCasillerosElProximoTurnoEsElAZUL() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
-		juego.soltarFicha(2);
 
-		Assert.assertEquals(Casillero.AMARILLO, juego.obtenerCasillero(1, 1));
-		Assert.assertEquals(Casillero.ROJO, juego.obtenerCasillero(2, 1));
-		Assert.assertEquals(Casillero.AMARILLO, juego.obtenerCasillero(3, 1));
-		Assert.assertEquals(Casillero.ROJO, juego.obtenerCasillero(4, 1));
-		Assert.assertEquals(Casillero.ROJO, juego.obtenerCasillero(4, 2));
-
-	}
-
-	@Test
-	public void PrueboQueSiComienzaElRojoYCompletaUnaColumnaDeCincoCasillerosElProximoTurnoEsElAmarillo() {
-
-		CuatroEnLinea juego = new CuatroEnLinea(5, 4, "Rojo", "Amarillo");
-		
-		juego.soltarFicha(1);
-		juego.soltarFicha(1);
-		juego.soltarFicha(1);
-		juego.soltarFicha(1);
-		juego.soltarFicha(1);
-		juego.soltarFicha(2);
-
-		Assert.assertEquals(Casillero.ROJO, juego.obtenerCasillero(1, 1));
-		Assert.assertEquals(Casillero.AMARILLO, juego.obtenerCasillero(2, 1));
-		Assert.assertEquals(Casillero.ROJO, juego.obtenerCasillero(3, 1));
-		Assert.assertEquals(Casillero.AMARILLO, juego.obtenerCasillero(4, 1));
-		Assert.assertEquals(Casillero.ROJO, juego.obtenerCasillero(5, 1));
-		Assert.assertEquals(Casillero.AMARILLO, juego.obtenerCasillero(5, 2));
+		Assert.assertEquals(Casillero.AZUL,
+				juego.obtenerCasilleroDelJugadorConTurno());
 
 	}
 
 	@Test
 	public void PrueboQueAlLlenarElTableroSinHacerCuatroEnLineaElJuegoTerminoYNoHayGanador() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 
 		Assert.assertFalse(juego.termino());
 
 		for (int i = 1; i < 3; i++) {
-			juego.soltarFicha(1);		
-			juego.soltarFicha(2);		
-			juego.soltarFicha(3);		
+			juego.soltarFicha(1);
+			juego.soltarFicha(2);
+			juego.soltarFicha(3);
 			juego.soltarFicha(4);
 		}
-		
+
 		for (int i = 1; i < 3; i++) {
 			juego.soltarFicha(4);
 			juego.soltarFicha(3);
@@ -223,9 +241,9 @@ public class CuatroEnLineaTest {
 	}
 
 	@Test
-	public void PrueboQueGanaElJugadorRojoHaciendoCuatroEnLineaDeFormaHorizontalEnUltimaFila() {
+	public void PrueboQueGanaElJugadorAzulHaciendoCuatroEnLineaDeFormaHorizontalEnUltimaFila() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
 		juego.soltarFicha(2);
@@ -235,14 +253,14 @@ public class CuatroEnLineaTest {
 		juego.soltarFicha(4);
 
 		Assert.assertTrue(juego.hayGanador());
-		Assert.assertEquals("Rojo", juego.obtenerGanador());
+		Assert.assertEquals("Azul", juego.obtenerGanador());
 		Assert.assertTrue(juego.termino());
 	}
 
 	@Test
-	public void PrueboQueElJugadorRojoNoHaceCuatroEnLineaHorizontalEnLaUltimaFilaEntoncesNoHayGanador() {
+	public void PrueboQueElJugadorAzulNoHaceCuatroEnLineaHorizontalEnLaUltimaFilaEntoncesNoHayGanador() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
@@ -256,9 +274,9 @@ public class CuatroEnLineaTest {
 	}
 
 	@Test
-	public void PrueboQueGanaElJugadorRojoHaciendoCuatroEnLineaDeFormaVerticalEnLaPrimerColumna() {
+	public void PrueboQueGanaElJugadorAzulHaciendoCuatroEnLineaDeFormaVerticalEnLaPrimerColumna() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 		juego.soltarFicha(1);
 		juego.soltarFicha(2);
 		juego.soltarFicha(1);
@@ -268,14 +286,14 @@ public class CuatroEnLineaTest {
 		juego.soltarFicha(1);
 
 		Assert.assertTrue(juego.hayGanador());
-		Assert.assertEquals("Rojo", juego.obtenerGanador());
+		Assert.assertEquals("Azul", juego.obtenerGanador());
 		Assert.assertTrue(juego.termino());
 	}
 
 	@Test
-	public void PrueboQueNoGanaElJugadorRojoPorqueNoHaceCuatroEnLineaDeFormaVerticalEnLaPrimerColumna() {
+	public void PrueboQueNoGanaElJugadorAZULPorqueNoHaceCuatroEnLineaDeFormaVerticalEnLaPrimerColumna() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 		juego.soltarFicha(1);
 		juego.soltarFicha(3);
 		juego.soltarFicha(1);
@@ -289,9 +307,9 @@ public class CuatroEnLineaTest {
 	}
 
 	@Test
-	public void PrueboQueGanaElJugadorRojoHaciendoCuatroEnLineaDiagonalEmpezandoElPrimeroPorAbajo() {
+	public void PrueboQueGanaElJugadorAzulHaciendoCuatroEnLineaDiagonalEmpezandoElPrimeroPorAbajo() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 		juego.soltarFicha(1);
 		juego.soltarFicha(2);
 		juego.soltarFicha(2);
@@ -305,14 +323,14 @@ public class CuatroEnLineaTest {
 		juego.soltarFicha(4);
 
 		Assert.assertTrue(juego.hayGanador());
-		Assert.assertEquals("Rojo", juego.obtenerGanador());
+		Assert.assertEquals("Azul", juego.obtenerGanador());
 		Assert.assertTrue(juego.termino());
 	}
 
 	@Test
 	public void PrueboQueGanaElJugadorAmarilloHaciendoCuatroEnLineaDiagonalEmpezandoElPrimeroPorArriba() {
 
-		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Rojo", "Amarillo");
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
 		juego.soltarFicha(1);
@@ -330,4 +348,41 @@ public class CuatroEnLineaTest {
 		Assert.assertEquals("Amarillo", juego.obtenerGanador());
 		Assert.assertTrue(juego.termino());
 	}
+
+	@Test(expected = Error.class)
+	public void PrueboErrorSoltarFichaFueraDeRangoMatriz4x4DevuelveUnError() {
+		// creo un tablero de 4x4, suelto 4 fichas, y al intentar soltar la
+		// quinta tira error
+
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
+
+		juego.soltarFicha(1);
+		juego.soltarFicha(1);
+		juego.soltarFicha(1);
+		juego.soltarFicha(1);
+		juego.soltarFicha(1);
+
+		Assert.assertEquals(1, juego.obtenerContadorErrorCasilleroLleno());
+
+	}
+
+	@Test(expected = Error.class)
+	public void PrueboErrorSoltarFichaFueraDeRangoMatriz4x4Devuelve4Errores() {
+		// creo un tablero de 4x4, suelto 4 fichas, y al intentar soltar la
+		// quinta tira error
+
+		CuatroEnLinea juego = new CuatroEnLinea(4, 4, "Azul", "Amarillo");
+
+		juego.soltarFicha(1);
+		juego.soltarFicha(1);
+		juego.soltarFicha(1);
+		juego.soltarFicha(1);
+		juego.soltarFicha(1);
+		juego.soltarFicha(1);
+		juego.soltarFicha(1);
+		juego.soltarFicha(1);
+
+		Assert.assertEquals(4, juego.obtenerContadorErrorCasilleroLleno());
+	}
+
 }
